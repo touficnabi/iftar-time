8e6c35d157a510d4571baeaf550b2ceeca80cb73
touficnabi <touficnabi@gmail.com>
2025-03-04 01:40:08 +0600

    city name to lat long

--- src/component/manualLocation.js
+++ src/component/manualLocation.js
@@ -6,11 +6,17 @@ const ManualLocation = ({onManualLocationSelection}) => {
     const [countries, setCountries] = useState(null);
     const [country, setCountry] = useState(null);
     const [cities, setCities] = useState();
-    const [selectedCity, setSelectedCity] = useState(null)
+    const [selectedCity, setSelectedCity] = useState(null);
 
-    const handleManualLocationSelection = () => {
+    const handleManualLocationSelection = async () => {
+        const res = await axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${selectedCity},${country}&limit=1&appid=${process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY}`);
+        if (res.data[0]) {
+            const {lat, lon} = res.data[0];
+            onManualLocationSelection(selectedCity, country, lat, lon);
+        } else {
+            console.log('error in finding the lat long from the city')
+        }
         setOpen(false)
-        onManualLocationSelection(selectedCity, country);
     }
 
     const handleManualLocationTrigger = () => {
@@ -19,7 +25,7 @@ const ManualLocation = ({onManualLocationSelection}) => {
     
     const fetchCities = async country => {
         setCountry(country);
-        setCities([]); //to clean up previous selection
+        setCities(null); //to clean up previous selection
         setSelectedCity(""); //reset previous city
 
         try {
@@ -40,23 +46,26 @@ const ManualLocation = ({onManualLocationSelection}) => {
         <>
             <button className='manual-location-trigger' onClick={handleManualLocationTrigger}>{open ? 'Close' : 'Change Location'}</button>
             <div className={`manual-location-selction ${open ? 'open' : ''}`}>
-            {countries && <select onChange={(e)  => fetchCities(e.target.value)} name="country" id="country">
-                <option value="">Select a country</option>
-                {countries.map(country => (
-                    <option key={country.iso3} value={country.country}>
-                        {country.country}
-                    </option>
-                ))}
-            </select>}
+                    <h2>Select your location</h2>
+                <div className="container">
+                    {countries && <select onChange={(e)  => fetchCities(e.target.value)} name="country" id="country">
+                        <option value="">Select a country</option>
+                        {countries.map(country => (
+                            <option key={country.iso3} value={country.country}>
+                                {country.country}
+                            </option>
+                        ))}
+                    </select>}
 
-            {cities && <select value={selectedCity} onChange={(e) => setSelectedCity(e.target.value)}>
-                <option value="">Select your city</option> 
-                {cities.map(city => (
-                    <option key={city} value={city}>{city}</option>
-                ))}
-            </select>}
+                    {cities && <select value={selectedCity} onChange={(e) => setSelectedCity(e.target.value)}>
+                        <option value="">Select your city</option> 
+                        {cities.map(city => (
+                            <option key={city} value={city}>{city}</option>
+                        ))}
+                    </select>}
 
-            {selectedCity && <button onClick={handleManualLocationSelection}>Confirm</button>}
+                    {selectedCity && <button onClick={handleManualLocationSelection}>Confirm</button>}
+                </div>
                 
             </div>
         </>
