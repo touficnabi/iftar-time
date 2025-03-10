0ba11f18318bb71ab13350ac02c2659f3e7ff400
Toufic Nabi <touficnabi@gmail.com>
2025-03-11 05:47:13 +0600

    header update

--- src/component/Header.js
+++ src/component/Header.js
@@ -4,9 +4,9 @@ const Header = ({city,  methods, setMethod, defaultMethod}) => {
         <header>
             <div className="header-wrapper">
                 {/* <h1>Iftar and Sehri Time in <u>{city}</u></h1> */}
-                <h1>Iftar and Sehri Time {city && `in ${city}`}</h1>
+                <h1>🕌 Iftar and Sehri Time {city && `in ${city}`}</h1>
                 <div className="method">
-                    {/* <h3>Choose a method: </h3> */}
+                    <h3>Source: </h3>
                     <select onChange={setMethod} name="method" defaultValue={defaultMethod}>
                         {methods.map(method => method.name && <option key={method.id} value={method.id}>{method.name}</option>)}
                     </select>
