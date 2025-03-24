c16ed7ed61bc06a9f3feb0b81f115b5ee541efa3
Toufic Nabi <touficnabi@gmail.com>
2025-03-24 21:13:28 +0600

    text update

--- src/component/MultiCity.js
+++ src/component/MultiCity.js
@@ -3,7 +3,7 @@ import { CiCircleRemove } from "react-icons/ci";
 const MultiCity = ({multiCities, saveCityInfo, country, handleManualLocationTrigger}) => {
     return (
         <div className="manual-location-selction">
-            <h2 className='manual-location-heading'>Select your location</h2>
+            <h2 className='manual-location-heading'>Select your city</h2>
             <div className="multi-city-selector">
                 <ul>
                     {multiCities.map(({name, state, lat, lon}) => <li key={state} onClick={() => saveCityInfo(name, country, lat, lon)}><b>{name}</b>{state}, {country.name}</li>)}  
